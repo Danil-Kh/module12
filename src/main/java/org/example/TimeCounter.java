ddaa6b8bd1e9d2ff5b7d0c4e7f34433a7f1321e1
Danil-Kh <164011772+Danil-Kh@users.noreply.github.com>
2024-07-09 22:23:24 +0300

    Create TimeCounter.java

---
+++ src/main/java/org/example/TimeCounter.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class TimeCounter {
+    public void timeAfterLaunch() {
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(2);
+        final long startTime = System.currentTimeMillis();
+
+        service.scheduleAtFixedRate(() -> {
+            long elapsedTime = (System.currentTimeMillis() - startTime) / 1000;
+            System.out.println("Часу пройшло с момента запуску: " + elapsedTime + " секунд");
+        }, 0, 1, TimeUnit.SECONDS);
+
+        service.scheduleAtFixedRate(() ->
+                        System.out.println("Пройшло 5 секунд"),
+                0, 5, TimeUnit.SECONDS);
+    }
+}
